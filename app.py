e7091cc516b725448bb60bf444eb735e9db1ef3d
Yaroslav <y2020postolatiy@gmail.com>
2022-09-23 16:06:32 +0300

    render_template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/app/templates')
 def index():
-    return 'Hello Yarik'
+    return render_template('index.html', title='Welcome')
 
 app.run(host='0.0.0.0', port=5002)
-
-

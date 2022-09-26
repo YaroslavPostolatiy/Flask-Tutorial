b634abbcf30003702569357bf27dd2ff8c530aa8
Yaroslav <y2020postolatiy@gmail.com>
2022-09-26 11:36:40 +0300

    add init.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
-app.run(host='0.0.0.0', port=5018)
+app.run(host='0.0.0.0', port=5000)
